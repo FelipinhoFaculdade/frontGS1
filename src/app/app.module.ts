2ed9542084fb81e962aa63dbdd2e85c0fbb5c616
L1P3Z1NH0 <felipecarrapeiro@hotmail.com>
2024-03-29 20:14:15 -0300

    fix: home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {
+  faChevronRight,
+  faFire,
+  faPersonFallingBurst,
+  faWind,
+} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,4 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFire, faWind, faPersonFallingBurst, faChevronRight);
+  }
+}
